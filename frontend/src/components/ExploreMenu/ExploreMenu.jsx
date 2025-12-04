da06ad420a4f12b778fc75202b131489ec5b7186
sps72057931 <sps72057931@gmail.com>
2025-12-04 18:18:11 +0530

    fifth

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/frontend_assets/assets";
 
-const ExploreMenu = ({category,setCategory}) => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
@@ -11,17 +11,29 @@ const ExploreMenu = ({category,setCategory}) => {
         mission is to satisfy your cravings and elevate your dining experience,
         one delicious meal at a time.
       </p>
+
       <div className="explore-menu-list">
-        {menu_list.map((item, index) => {
-          return (
-            <div onClick={()=>setCategory(prev=>prev===item.menu_image.name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
-              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
-              <p>{item.menu_name}</p>
-            </div>
-          );
-        })}
+        {menu_list.map((item, index) => (
+          <div
+            key={index}
+            className="explore-menu-list-item"
+            onClick={() =>
+              setCategory(prev =>
+                prev === item.menu_name ? "All" : item.menu_name
+              )
+            }
+          >
+            <img
+              className={category === item.menu_name ? "active" : ""}
+              src={item.menu_image}
+              alt=""
+            />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
       </div>
-      <hr/>
+
+      <hr />
     </div>
   );
 };
