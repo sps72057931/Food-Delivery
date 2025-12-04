04750ac8755d063d6c50f09057c7db2bdd84776b
sps72057931 <sps72057931@gmail.com>
2025-12-04 18:38:35 +0530

    eigth

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -14,7 +14,7 @@ const Cart = () => {
     url
   } = useContext(StoreContext);
 
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -29,17 +29,21 @@ const Cart = () => {
         </div>
         <br />
         <hr />
+
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div>
+              <div key={item._id}>
                 <div className="cart-items-title cart-items-item">
-                  <img src={url+"/images/"+item.image} alt="" />
+                  <img src={url + "/images/" + item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>${item.price}</p>
+                  <p>₹{item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>${item.price * cartItems[item._id]}</p>
-                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                  <p>₹{item.price * cartItems[item._id]}</p>
+                  <p
+                    onClick={() => removeFromCart(item._id)}
+                    className="cross"
+                  >
                     x
                   </p>
                 </div>
@@ -47,38 +51,51 @@ const Cart = () => {
               </div>
             );
           }
+          return null;
         })}
       </div>
+
       <div className="cart-bottom">
         <div className="cart-total">
           <h2>Cart Totals</h2>
+
           <div>
             <div className="cart-total-details">
               <p>Subtotals</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>₹{getTotalCartAmount()}</p>
             </div>
+
             <hr />
+
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>₹{getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
+
             <hr />
+
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              <b>₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+
+          <button onClick={() => navigate("/order")}>
+            PROCEED TO CHECKOUT
+          </button>
         </div>
+
         <div className="cart-promocode">
           <div>
             <p>If you have a promocode, Enter it here</p>
+
             <div className="cart-promocode-input">
               <input type="text" placeholder="promo code" />
               <button>Submit</button>
             </div>
           </div>
         </div>
+
       </div>
     </div>
   );
